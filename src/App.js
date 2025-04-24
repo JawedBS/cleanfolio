aa996ffd6585a85df7d601c9f0887fb58e0e182c
JawedBS <145364921+JawedBS@users.noreply.github.com>
2025-04-24 05:42:13 +0200

    ajout d'un console log

--- src/App.js
+++ src/App.js
@@ -6,12 +6,25 @@ import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
+import Extra from './components/Extra/Extra'
 import Footer from './components/Footer/Footer'
 import './App.css'
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext)
 
+  console.log(
+    "%c👋 Hello développeur curieux !",
+    "color: #61dafb; font-size: 18px; font-weight: bold;"
+  )
+  console.log(
+    "%cTu veux voir le code ? Il est ici 👉 github.com/JawedBS",
+    "color: #9ae6b4; font-size: 14px;"
+  )
+  console.log(
+    "%cPS : chaque pixel ici a été pensé avec passion ✨",
+    "color: #fbd38d; font-style: italic;"
+  )
   return (
     <div id='top' className={`${themeName} app`}>
       <Header />
@@ -20,6 +33,7 @@ const App = () => {
         <About />
         <Projects />
         <Skills />
+        <Extra />
         <Contact />
       </main>
 
