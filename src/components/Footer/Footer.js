3eec9061e3f023a4b455ae223d1e83734f26b007
JawedBS <145364921+JawedBS@users.noreply.github.com>
2025-06-07 07:53:30 +0200

    modif package

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/JawedBS/cleanfolio.git'
       className='link footer__link'
     >
       Portfolio de Jawed Ben salah
